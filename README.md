68c3944c45524ab658cdf16cad6b2856e8761467
Ivan Duka <39438519+ivanduka@users.noreply.github.com>
2021-09-01 21:19:30 -0600

    updates

---
+++ README.md
@@ -0,0 +1,3 @@
+python -m venv env1
+env1/Scripts/activate
+pip install -r requirements.txt
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import io
 from dotenv import load_dotenv
 import os
 
+# Load environmental variables from .env file if running locally
 load_dotenv()
 
 # Define parameters
@@ -19,18 +20,25 @@ outputBlobName = "iris_setosa.csv"
 # Establish connection with the blob storage account
 client = BlobServiceClient.from_connection_string(conn_str=connectionString)
 
+# Read CSV as a string
 csv = (
     client.get_blob_client(blob=inputBlobName, container=inputContainerName)
     .download_blob()
     .readall()
     .decode("utf-8")
 )
+
+# Load CSV as a string to a dataframe. Note the convertion to io.String
 df = pd.read_csv(io.StringIO(csv))
 
+# Filtering by column value
 df = df[df["Species"] == "setosa"]
 
+# Uploading resulting CSV
+new_csv = df.to_csv()
+
 client.get_blob_client(blob=outputBlobName, container=outputContainerName).upload_blob(
-    df.to_csv(), overwrite=True
+    new_csv, overwrite=True
 )
 
 print("Success!")
